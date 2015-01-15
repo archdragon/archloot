c37ffed2d02abc0bcf18d2f9ccb9fd219e3b1eef
Przemek <przemek@krowinski.eu>
2015-01-15 15:37:58 +0100

    Export RNG into a separate class

---
+++ lib/archloot/random_number_generator.rb
@@ -0,0 +1,9 @@
+# This class copies basic random number generation and can be easily with your own implementation.
+
+module Archloot
+  class RandomNumberGenerator
+    def rand
+      Random.rand
+    end
+  end
+end
