891230d63da3d618319183d5b7dfae2c10b74448
Przemek <przemek@krowinski.eu>
2015-01-28 04:24:33 +0100

    Update the factory to accept hash parameters

--- spec/factories/drop.rb
+++ spec/factories/drop.rb
@@ -1,13 +1,19 @@
 FactoryGirl.define do
   factory :drop, :class => Archloot::Drop do
     initialize_with do
-      new("Magical ring of Spec")
-    end    
-    factory :drop_lucky do
-      chance 1
+      new({item: "Magical ring of Spec"})
     end
-    factory :drop_unlucky do
-      chance 0
+  end
+
+  factory :drop_lucky, :class => Archloot::Drop do
+    initialize_with do
+      new({item: "Lucky Item", chance: 1})
+    end
+  end
+
+  factory :drop_unlucky, :class => Archloot::Drop do
+    initialize_with do
+      new({item: "Lucky Item", chance: 0})
     end
   end
 end
