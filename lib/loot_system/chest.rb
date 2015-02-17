67074043c9e79f1f179e16094812fb2a836f0a02
Przemek <hello@krowinski.com>
2015-02-17 17:41:05 +0100

    Remove unnecessy variables

--- lib/loot_system/chest.rb
+++ lib/loot_system/chest.rb
@@ -1,10 +1,9 @@
 module LootSystem
   class Chest
-    attr_accessor :drops_limit
+    # List of all the drops that have a chance to be spawned in this chest
     attr_reader :potential_droplist
 
     def initialize
-      @drops_limit = 1
       @potential_droplist = Droplist.new
     end
 
@@ -22,18 +21,14 @@ module LootSystem
 
     # Get a list of all drops that will be spawned by the chest
     def generate_final_droplist
-      drops_count = 0
-
       @final_droplist = Droplist.new
-
       @potential_droplist.each { |drop| process_potential_drop(drop) }
-
-      @final_droplist
+      return @final_droplist
     end
 
     # Check if the potential drop will be given to the player after opening a chest
     def process_potential_drop(drop)
-      if drop.successful? && @final_droplist.length < @drops_limit
+      if drop.successful?
         @final_droplist.add(drop)
       end
     end
