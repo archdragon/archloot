2711dde4da4b8a21e0fe50e6e0423df407c7d56d
Przemek <hello@krowinski.com>
2015-02-22 00:41:51 +0100

    Add min and max params

--- lib/loot_system/drop.rb
+++ lib/loot_system/drop.rb
@@ -13,10 +13,11 @@ module LootSystem
     #
     #   @magical_item = Item.new({id: 113})
     #   Drop.new({item: @magical_item})
-    def initialize(item:, chance: 1, random_number_generator: RandomNumberGenerator.new, hours: (0..23))
+    def initialize(item:, chance: 1, random_number_generator: RandomNumberGenerator.new, hours: (0..23), min: 1, max: 1)
       @item = item
       @random_number_generator = random_number_generator
       @chance = chance
+      @count = count(min, max)
     end
 
     # Returns true if this Drop will be spawned.
@@ -34,6 +35,10 @@ module LootSystem
     def valid_hour?
       t = Time.now
       @hours === t.hour
+    end
+
+    def count(min, max)
+      (min..max).to_a.sample
     end
   end
 end
